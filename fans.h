2d5d47b2048ce722a89953b0c8af092a31c5591d
Sabir777 <sabir.yanin2014@yandex.ru>
2023-06-22 22:14:45 +0300

    Добавил таймеры и вентиляторы. Осталось добавить реализацию вентилятора

---
+++ fans.h
@@ -0,0 +1,13 @@
+
+
+enum class Bathroom{
+  DRY, WET
+};
+
+enum class Toilet{
+  FRESH, STINK
+};
+
+class Fan{
+  Fan(int);
+}
--- timer.cpp
+++ timer.cpp
@@ -1,7 +1,13 @@
 #include timer.h
 
-Timer::Timer(int dt):dt{dt}{ }
+Timer::Timer(int dt):dt{dt}{
+  this->start();
+}
 
 void Timer::start(){
-  this.t0 = millis();
+  this->t0 = millis();
+}
+
+bool Timer::is_time(){
+  return (millis() - t0) >= dt;
 }
